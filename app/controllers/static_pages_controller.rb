65c63c0e6a981b2610a2ae804bd50fb616f62c91
Serge <depotstoreinfo@gmail.com>
2017-10-06 01:16:13 +0300

    Answer to comments

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,17 @@
 class StaticPagesController < ApplicationController
-
   def home
     @card = Card.review_date_over(Time.now).random_card
-    session[:checked_card_id] = @card.id
   end
 
   def check_answer
-    @card = Card.find(session[:checked_card_id])
+    @card = Card.find(params[:checked_card_id])
     if @card.check_translation(params[:translation])
       flash[:success] = 'Правильно'
-      session.delete(:checked_card_id)
-      @card.set_review_date(Time.now)
+      @card.arrange_review_date(Time.now)
       redirect_to root_path
     else
       flash[:danger] = 'Не правильно'
       render 'static_pages/home'
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   post 'static_pages/check_answer'
-  get 'static_pages/check_answer', to: 'static_pages#home' # display 'home' form when browser reloaded
+  # display 'home' form when browser reloaded
+  get 'static_pages/check_answer', to: 'static_pages#home'
   resources :cards, except: [:show]
 end
