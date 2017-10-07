b280eca1bb9f6c32e7abee9ec98c1b8b463357fa
Serge <depotstoreinfo@gmail.com>
2017-10-07 23:10:52 +0300

    Compete fifth task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   post 'static_pages/check_answer'
    # displays 'home' form when browser reloaded
-  get 'static_pages/check_answer', to: 'static_pages#home'
+  get 'static_pages/check_answer', to: 'static_pages#home' #code debt
   resources :cards, except: %i[show]
 end
